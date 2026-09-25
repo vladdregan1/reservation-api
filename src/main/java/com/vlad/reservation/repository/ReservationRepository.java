ba26c92c61e889096644f4543d353e046ceedfb8
Vlad Dregan <vladdregan2004a@gmail.com>
2026-09-25 13:03:34 +0300

    add ReservationRepository

---
+++ src/main/java/com/vlad/reservation/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.vlad.reservation.repository;
+
+import com.vlad.reservation.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
